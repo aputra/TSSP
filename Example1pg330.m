c32f80189414e0c81d752cbc8ffce109258eb312
aputra <andika28putra@Andikas-MacBook-Pro.local>
2014-05-29 14:41:27 -0400

    2014_05_29
    1-D TSSP. Confirmed benchmark with the references: J Comp Phys 187
    (2003) and J Comp Phys 175 (2002). There is typo in Eq (4.3) for 1st
    reference which causes confusion in 2014_05_27 program

--- Example1pg330.m
+++ Example1pg330.m
@@ -3,53 +3,56 @@
 
 clear all; close all; clc;
 
-vareps = 0.1;
+vareps = 0.025;
 kappa1 = 1.2649;
-a = -16; b =16;
-M = 16*32;
-h = 1/16;          % h is the mesh size: h = (b-a)/M
-k = 0.02;
+a = -16; b = 16;
+
+h = 1/64;          % h is the mesh size: h = (b-a)/M
+M = round(1/h*(b-a));
+k = 0.0025;
 
 x = a:h:b;
-% x = (a+h/2):h:(b-h/2);
 l = -M/2:(M/2-1);
 j_id = 0:(M-1);
 mu_l = 2*pi*l/(b-a);
 
-psi_x = 1/((pi*vareps)^(1/4))*exp(-x.^2/2/vareps);
-Nk = 2000;
-idd = intersect(find(x<8), find(x>-8));
+% S0 = -log(exp(x)+exp(-x));
+% psi_x = exp(-x.^2).*(1+0.2*cos(x/sqrt(vareps)).*cos(x/sqrt(vareps))).*exp(1i/vareps.*S0);
+psi_x = 1/(pi)^(1/4)*exp(-x.^2/2);
+
+Nk = 5/k;
+idd = intersect(find(x<=3), find(x>=0));
 
 abc = zeros(length(idd),Nk);
 sigm_w = zeros(size(1:Nk));
+x_com = zeros(size(1:Nk));
+
+% F(Nk) = struct('cdata',[],'colormap',[]);
 
 for kk = 1:Nk
-        psi_star = exp(-1i* (x.^2/2+kappa1*(psi_x).*(conj(psi_x)))...
-                            *k/2/vareps)                            .*psi_x;
+    psi_star = exp(-1i* (x.^2/2 + kappa1*abs(psi_x).^2)...
+                        *k/2/vareps)                            .*psi_x;
 
-        psi_starHat = zeros(size(l));
-        psi_doubleStar = zeros(size(j_id));
+    
+    psi_starHat = psi_star(1:M)*exp(-1i*(x(1:M)-a)'*mu_l);
+    psi_doubleStar = 1/M * (exp(-1i*vareps*k*mu_l.^2/2).*psi_starHat) * exp(1i*mu_l'*(x(1:M)-a));
+    
+    psi_x(1:length(j_id)) = exp(-1i*(x(1:M).^2/2+kappa1*abs(psi_doubleStar).^2)*k/2/vareps).*psi_doubleStar;
+    psi_x(M+1)=psi_x(1);
 
-        for ll = 1:length(l)
-            psi_starHat(ll) = sum(psi_star(1:M).*exp(-1i*mu_l(ll)*(x(1:M)-a)));
-        end
+    abc(:,kk) = abs(psi_x(idd)).^2;
+    aa = abs(psi_x).^2;
 
-        for jj = 1:length(j_id)
-            psi_doubleStar(jj) = 1/M*sum(exp(-1i*vareps*k*mu_l.^2/2).*psi_starHat.*exp(1i*mu_l*(x(jj)-a)));
-            psi_x(jj) = exp(-1i*(x(jj)^2/2+kappa1*abs(psi_doubleStar(jj))^2)*k/2/vareps)*psi_doubleStar(jj);
-        end
-        psi_x(M+1)=psi_x(1);
-%         psi_x = exp(-1i*(x.^2/2+kappa1*abs(psi_doubleStar).^2)*k/2/vareps).*psi_doubleStar;
-        abc(:,kk) = abs(psi_x(idd)).^2;
-        aa = abs(psi_x).^2;
-        
-        x_av = sum(x.*aa*h);
-        sigma_sq = sum(((x-x_av).^2).*aa*h);
-% %         
-% %         ia = max(aa);
-% %         idx = intersect(find(aa<(ia/exp(1)+0.005)),find(aa>(ia/exp(1)-0.005)));
-        sigm_w(kk) = sqrt(sigma_sq);
+    x_av = sum(x.*aa*h);
+    x_com(kk) = x_av;
+    sigma_sq = sum(((x).^2).*aa*h);
+
+    sigm_w(kk) = sqrt(sigma_sq);
+%     figure(1);
+%         plot(x(idd),abc(:,kk));
+    
 end
 
-figure; plot((1:Nk)*k,sigm_w,'b-');
-% figure;plot(x(201:313),abs(psi_x(201:313)).^2)
+
+figure(2); plot((1:Nk)*k,sigm_w,'b-');
+figure(3); plot(x(idd),abc(:,kk),'b+-');
